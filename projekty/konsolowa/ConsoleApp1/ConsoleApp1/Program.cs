14da071a6e92fd3fe70c78f96eda30389431053e
q <q>
2024-11-27 12:48:34 +0100

    add

---
+++ projekty/konsolowa/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        /**
+         * <Najlpeszy czas>
+         * <Liczy najmniejsza czas w przedzial  w podanej ilosci kilometrow>
+         * Parametry wejściowe:
+         * <wielksoc> - przedial kilometrow>
+         * <czasy> - <czasy przebiegane w kazdym kilometerze>
+         * ...
+         * wartość zwracana:
+         * <opis zwraca najmniejszy czas w przedziale w podanej ilosci kilometrow>
+         * autor: Kacper Petelicki>
+         **/
+        int NajlepszyCzas(int wielkość, int[]czasy)
+        {
+            int suma = 0;
+            int maxSuma = int.MaxValue;
+            for (int i = 0; i < czasy.Length; i++)
+            {
+                suma = 0;
+                for (int j = 0; j < wielkość;j++)
+                {
+                    if (czasy.Length <= (i + j))
+                    {
+                        suma = maxSuma;
+
+                    }
+                    else
+                    {
+
+                        suma += czasy[i + j];
+                    } 
+
+                
+                }
+                if (suma < maxSuma)
+                {
+                    maxSuma = suma;
+                }
+            }
+            return maxSuma;
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();    
+            Console.WriteLine("Ile kilometrów przebiegłeś");
+            if(int.TryParse(Console.ReadLine(), out int km))
+            {
+                int[]czasy = new int[km];
+                Console.WriteLine("Podaj czasy na każdym kilometrze: ");
+                for (global::System.Int32 i = 0; i < czasy.Length; i++)
+                {
+                    czasy[i] = int.Parse(Console.ReadLine());
+                }
+               
+                Console.WriteLine("Podaj jakiej wielkości najlepszego odcinka szukasz (w km): ");
+                if (int.TryParse(Console.ReadLine(),out int wielkość))
+                {
+                    Console.WriteLine(program.NajlepszyCzas(wielkość,czasy).ToString());
+                }
+                
+                {
+                    
+                }
+            }
+            else
+            {
+                Console.WriteLine("Błąd");
+                return; 
+            }
+        }
+    }
+}
---
+++ projekty/mobilna/MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        public int Minuty { get; set; }
+        public int Sekundy { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Minuty = 0;
+            Sekundy = 0;
+        }
+
+
+
+        private void ButtonClickedReset(object sender, EventArgs e)
+        {
+            lacznyCzas.Text = "0:00";
+            Minuty = 0;
+            Sekundy = 0;
+        }
+
+        private void ButtonClickedDodajCzas(object sender, EventArgs e)
+        {
+            if(int.TryParse(minuty.Text, out int minuta)&& int.TryParse(sekundy.Text, out int sekunda ))
+            {
+                
+                Sekundy+= sekunda;
+                Minuty+= minuta;
+                if(Sekundy>59)
+                {
+                    Minuty += Sekundy / 60;
+                    Sekundy = Sekundy % 60;
+                   
+                    
+                }
+                if (Sekundy<10)
+                {
+                    lacznyCzas.Text = Minuty.ToString() + ":" + "0"+ Sekundy.ToString();
+                    return;
+                }
+                lacznyCzas.Text = Minuty.ToString() + ":" + Sekundy.ToString();
+                return;
+            }
+            else
+            {
+                DisplayAlert("błąd", "Błedna Wartość", "OK");
+                return;
+            }
+           
+        }
+    }
+
+}
